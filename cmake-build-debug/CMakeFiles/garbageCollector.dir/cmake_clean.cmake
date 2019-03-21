4231bd2be23bab4a0f09c0c531e3e8b82b090073
eric <erickrim@yahoo.co.uk>
2019-03-21 11:30:50 +0000

    intial commit

---
+++ cmake-build-debug/CMakeFiles/garbageCollector.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/garbageCollector.dir/main.cpp.o"
+  "garbageCollector.pdb"
+  "garbageCollector"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/garbageCollector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
